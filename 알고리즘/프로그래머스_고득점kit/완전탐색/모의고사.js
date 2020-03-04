e4fe4cce987735550679dcec1b814dd815709fd7
WooJongSeon <jswoo030@gmail.com>
2020-03-05 01:29:10 +0900

    forEach로 개선

--- 알고리즘/프로그래머스_고득점kit/완전탐색/모의고사.js
+++ 알고리즘/프로그래머스_고득점kit/완전탐색/모의고사.js
@@ -7,39 +7,36 @@
 // 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
 //      3,3,1,1,2,2,4,4,5,5, 반복
 function solution(answers) {
-    var answer = [];
     let number1 = [1, 2, 3, 4, 5];
     let number2 = [2, 1, 2, 3, 2, 4, 2, 5];
     let number3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
     for (let i = 0; i < 2000; i++) {
         number1.push(...[1, 2, 3, 4, 5]);
-    }
-    for (let i = 0; i < 1250; i++) {
         number2.push(...[2, 1, 2, 3, 2, 4, 2, 5]);
-    }
-    for (let i = 0; i < 1000; i++) {
         number3.push(...[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]);
     }
     let count = [0, 0, 0];
-    for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === number1[i]) {
+    answers.forEach((value, index) => {
+        if (value === number1[index]) {
             count[0] += 1;
         }
-        if (answers[i] === number2[i]) {
+        if (value === number2[index]) {
             count[1] += 1;
         }
-        if (answers[i] === number3[i]) {
+        if (value === number3[index]) {
             count[2] += 1;
         }
-    }
+    });
     //최대값
     var max = count.reduce(function (previous, current) {
         return previous > current ? previous : current;
     });
-    return count.map((value) => { return value === max; })
+    let answer = count.map((value) => { return value === max; })
         .map((value, index) => { if (value) { return index + 1 } })
         .filter((value) => { return value !== undefined });
+    console.log(answer);
+    return answer;
 }
 
 solution([1, 2, 3, 4, 5]);
