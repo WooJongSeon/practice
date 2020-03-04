73c2c8b4278245706e7e009e18bdf910f85db83d
WooJongSeon <jswoo030@gmail.com>
2020-03-04 09:58:43 +0900

    문제 링크 추가

--- algorithm/js/programmerstop.js
+++ algorithm/js/programmerstop.js
@@ -1,27 +1,24 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42588
 function solution(heights) {
     var answer = [];
     const length = heights.length;
     for (let index = length; index > 0; index--) {
         const height = heights.pop();
-        if(index === 1) {
+        if (height < heights[heights.length - 1]) {
+            answer.unshift(index - 1);
+        } else if (height === heights[heights.length - 1]) {
             answer.unshift(0);
-        } else {
-            if(height < heights[heights.length - 1]) {
-                answer.unshift(index - 1);
-            } else if(height === heights[heights.length -1]) {
+        } else if (height > heights[heights.length - 1]) {
+            // pop 한 상태이므로 본인이 제일 큼
+            const exist = heights.find((h) => {
+                return h > height; // 현재 팝한 것보다 더 큰게 있다면?
+            })
+            if (exist) { // 있으면 있는것의 인덱스를 가져와야 함
+                answer.unshift(heights.lastIndexOf(exist) + 1);
+            } else { // 없다면
                 answer.unshift(0);
-            } else if(height > heights[heights.length -1]){
-                
-                // pop 한 상태이므로 본인이 제일 큼
-                const exist = heights.find((h) => {
-                    return h > height; // 현재 팝한 것보다 더 큰게 있다면?
-                })
-                if(exist) { // 있으면 있는것의 인덱스를 가져와야 함
-                    answer.unshift(heights.lastIndexOf(exist) +1);
-                } else { // 없다면
-                    answer.unshift(0);
-                }
             }
+
         }
     }
     console.log(answer);
@@ -34,8 +31,8 @@ function solution(heights) {
 // 9에 대해서 6이 9보다 작다. 다음 숫자는 없다. 0을 입력했다.
 // 6에 대해서 다음 숫자는 없다. 0을 입력했다.
 
-solution([6,9,5,7,4]);
-solution([3,9,9,3,5,7,2]);
+solution([6, 9, 5, 7, 4]);
+solution([3, 9, 9, 3, 5, 7, 2]);
 solution([7, 6, 5, 4, 3, 2, 1]);
 // 0 0 0 3 3 3 6
-solution([1,5,3,6,7,6,5]);
+solution([1, 5, 3, 6, 7, 6, 5]);
