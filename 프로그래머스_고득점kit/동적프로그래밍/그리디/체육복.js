7d3861c75b5fab48b2c6325def2f5358bf9e5c53
WooJongSeon <jswoo030@gmail.com>
2020-03-08 18:27:45 +0900

    코드 수정

--- 프로그래머스_고득점kit/동적프로그래밍/그리디/체육복.js
+++ 프로그래머스_고득점kit/동적프로그래밍/그리디/체육복.js
@@ -4,29 +4,41 @@
 // return 수업을 들을 수 있는 학생 수
 function solution(n, lost, reserve) {
     var answer = 0;
-    const arr = [1] * 10
-    console.log(arr);
-    const count = lost.length;
-    for (let i = 0; i < count; i++) {
-        const exist = reserve.indexOf(lost[i]);
-        if (exist > -1) {
-            reserve.splice(exist, 1);
-            lost.splice(reserve.indexOf(lost[i]), 1);
+    const arr = [];
+    for (let i = 0; i < n + 2; i++) {
+        arr.push(1);
+    }
+
+    lost.forEach((element) => {
+        arr[element] -= 1;
+    });
+
+    reserve.forEach((element) => {
+        arr[element] += 1;
+    });
+
+    for (let i = 1; i < n + 1; i++) {
+        if (arr[i - 1] === 0 && arr[i] === 2) { // 앞 사람에게 빌려주는 경우
+            arr[i - 1] += 1;
+            arr[i] -= 1;
+        }
+        else if (arr[i] === 2 && arr[i + 1] === 0) { // 뒷 사람에게 빌려주는 경우
+            arr[i] -= 1;
+            arr[i + 1] += 1;
         }
     }
 
-    console.log(lost);
-    console.log(reserve);
-    // 수업을 들을 수 있는 학생의 숫자 = n - lost.length + reverse.length;
-    answer = n - lost.length + reserve.length;
-    if (answer >= n) {
-        return n;
-    }
+    console.log(arr);
+    console.log(arr.length - 2)
+
+    arr.shift();
+    arr.pop();
+    answer = arr.filter((v) => {
+        return v !== 0;
+    }).length;
     return answer;
 }
 console.log(solution(7, [2, 3, 4], [1, 2, 3, 6]));
-//2,3,4
-//1,2,3,6
-// 같으면 remove 해서 없앤다. -> 빌려줄 수는 없고 일단 체육복을 갖고는 있는 학생
-// 그리고 잃어버린 학생 숫자 -> lost.length
-// 빌려줄 수 있는 학생 숫자 -> reserve.length
+// 1,1,1,1,1,1,1 배열에서 잃어버린 칸은 모두 -1을 한다
+// 여분이 있는 칸은 모두 +1 한다
+// 그리고 칸을 돌면서 n-1 이 0이고 n이 2이면 빌려준다.
