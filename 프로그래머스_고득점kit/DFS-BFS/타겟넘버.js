eca00ed40ba9221cbc052148cc1c3701c2a30179
WooJongSeon <jswoo030@gmail.com>
2020-03-10 08:24:17 +0900

    주석 추가

--- 프로그래머스_고득점kit/DFS-BFS/타겟넘버.js
+++ 프로그래머스_고득점kit/DFS-BFS/타겟넘버.js
@@ -1,7 +1,5 @@
 function solution(numbers, target) {
     var answer = 0;
-
-
     function recur(idx, sum) {
         if (idx === numbers.length) {
             if (sum === target) {
@@ -18,3 +16,6 @@ function solution(numbers, target) {
 
     return answer;
 }
+// 트리 구조를 활용해서 푼다.
+// 배열을 앞에서부터 하나씩 더했을 경우, 뺀 경우로 나눠서 트리를 뻗는다.
+// 뻗는 과정에서 target과 같은 숫자가 나온다면 카운트를 올린다
---
+++ 프로그래머스_고득점kit/DFS-BFS/타겟넘버.py
@@ -0,0 +1,8 @@
+def solution(numbers, target):
+    answer = 0
+    print(numbers[1:])
+    print(len(numbers))
+    return answer
+
+
+solution([1, 1, 1, 1, 1], 3)
