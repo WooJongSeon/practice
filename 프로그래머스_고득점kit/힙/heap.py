8cddee27cc6bffc21912b8a6f917804338b7192d
WooJongSeon <jswoo030@gmail.com>
2020-03-18 13:00:27 +0900

    프로그래머스 힙-더 맵게

---
+++ 프로그래머스_고득점kit/힙/heap.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+import heapq  # heapq.heappop 하면 알아서 제일 작은 것을 리턴한다
+
+
+def sol():
+    arr = [1, 2, 3, 4, 5]
+    heapq.heapify(arr)
+    heapq.heappush(arr, 10)
+    heapq.heappush(arr, 3)
+    heapq.heappush(arr, 5)
+    heapq.heappush(arr, 2)
+
+    print(arr)
+
+    print(arr)
+
+
+sol()
