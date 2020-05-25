276663efebeb2fa7293634a56fc8e9f41f29b526
WooJongSeon <jswoo030@gmail.com>
2020-05-25 10:02:39 +0900

    api-docs url 테스트

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -7,18 +7,17 @@ class WebsiteUser(HttpUser):
 
     @task(2)
     def index(self):
-        self.client.get("/")
-        self.client.get("/ajax-notifications/")
+        self.client.get("/api-docs")
 
-    @task(1)
-    def view_post(self):
-        post_id = random.randint(1, 10000)
-        self.client.get("/post?id=%i" % post_id, name="/post?id=[post-id]")
+    # @task(1)
+    # def view_post(self):
+    #     post_id = random.randint(1, 10000)
+    #     self.client.get("/post?id=%i" % post_id, name="/post?id=[post-id]")
 
-    def on_start(self):
-        """ on_start is called when a User starts before any task is scheduled """
-        self.login()
+    # def on_start(self):
+    #     """ on_start is called when a User starts before any task is scheduled """
+    #     self.login()
 
-    def login(self):
-        self.client.post(
-            "/login", {"username": "ellen_key", "password": "education"})
+    # def login(self):
+    #     self.client.post(
+    #         "/login", {"username": "ellen_key", "password": "education"})
