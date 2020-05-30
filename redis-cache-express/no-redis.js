5013bb21477c73c9bae46488d8a67d110b1cf2e6
WooJongSeon <jswoo030@gmail.com>
2020-05-30 12:24:11 +0900

    no redis 코드 복원

--- redis-cache-express/no-redis.js
+++ redis-cache-express/no-redis.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const cors = require('cors');
-app.use(cors());
+const PORT = process.env.PORT || 4000;
 app.get("/data", async (req, res) => {
   try {
-    const data = {
-      "id": 3,
-      "name": "이자바",
-      "email": "java@gmail.com",
-      "iat": 1590767182,
-      "exp": 1590770792,
-      "jti": "b3b84a09-759e-4ecc-a939-9932eabe0b9f"
-    }
-    return res.status(200).json({ result: data })
+    const result = await axios.get("https://api.spacexdata.com/v3/launches/latest")
+    return res.status(200).json({ result: result.data })
   } catch (error) {
     return Promise.reject(error);
   }
