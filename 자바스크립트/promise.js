dd5c416c53970f53e9adc974d253352a6e183177
WooJongSeon <jswoo030@gmail.com>
2020-05-31 21:22:15 +0900

    javascript 코드 추가

---
+++ 자바스크립트/promise.js
@@ -0,0 +1,8 @@
+setTimeout(function() { // (A)
+    console.log('A');
+}, 0);
+Promise.resolve().then(function() { // (B)
+    console.log('B');
+}).then(function() { // (C)
+    console.log('C');
+});
