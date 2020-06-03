da6011abb4f14a8ab43d899a48c5627944f39378
WooJongSeon <jswoo030@gmail.com>
2020-06-03 14:52:49 +0900

    object keys 활용

---
+++ javascript/object-key/object-keys.js
@@ -0,0 +1,18 @@
+const item = {
+  a: 'a',
+  b: 'b',
+  c: 'c',
+  d: 'd',
+  e: 'e',
+  f: 'f',
+  g: 'g',
+  h: 'h',
+  i: 'i',
+  j: 'j',
+}
+
+Object.keys(item).map((value) => {
+  if (value === 'a') {
+    console.log(value);
+  }
+})
