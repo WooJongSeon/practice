c8029753080006ffbb322849b91812347887e76c
WooJongSeon <jswoo030@gmail.com>
2020-06-05 14:16:29 +0900

    npm module test 추가

--- npm-test/index.js
+++ npm-test/index.js
@@ -1,1 +1,10 @@
-require('stock-cctx');
+const jsonIgnore = require('ignore-field');
+
+const objectToProcess = {
+  field1: 'field1',
+  field2: 'field2',
+  fieldToRemove: 'hihi'
+}
+
+const result = jsonIgnore(['fieldToRemove'], objectToProcess);
+console.log(result);
