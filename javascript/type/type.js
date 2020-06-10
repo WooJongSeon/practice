64c9d0cbba9f907396c34764c581cd18624da3ca
WooJongSeon <jswoo030@gmail.com>
2020-06-10 15:19:00 +0900

    typeof

--- javascript/type/type.js
+++ javascript/type/type.js
@@ -1,9 +1,19 @@
 const log = console.log;
 
-log(typeof undefined === "undefined"); // true
-log(typeof true === "boolean");// true
-log(typeof 1 === "number");// true
-log(typeof "1" === "string"); // true
-log(typeof { name: 1 } === "object"); // true
-log(typeof Symbol() === "symbol");// true
+// log(typeof undefined === "undefined"); // true
+// log(typeof true === "boolean");// true
+// log(typeof 1 === "number");// true
+// log(typeof "1" === "string"); // true
+// log(typeof { name: 1 } === "object"); // true
+// log(typeof Symbol() === "symbol");// true
 
+// log(typeof null === "object"); // true
+// var a = null;
+// log(!a && typeof a === "object");
+
+function index(params) {
+  this.name = 'index';
+
+}
+log(index.length)
+log(typeof [1, 2, 3, 4] === "object")
