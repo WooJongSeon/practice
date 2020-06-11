fa5250b67fc22c1565c63bf396f70777a5847022
WooJongSeon <jswoo030@gmail.com>
2020-06-11 14:02:47 +0900

    jsdoc

---
+++ javascript/jsdoc/def.js
@@ -0,0 +1,12 @@
+// Taken from example/v2/swaggerDef.js
+
+module.exports = {
+  info: {
+    // API informations (required)
+    title: 'Hello World', // Title (required)
+    version: '3.0.0', // Version (required)
+    description: 'A sample API', // Description (optional)
+  },
+  // host, // Host (optional)
+  basePath: '/', // Base path (optional)
+};
---
+++ javascript/jsdoc/index.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ * @returns {object} data
+ */
+const Book = (title, author) => {
+
+}
