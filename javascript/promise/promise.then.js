464d3447a40d6d664968f1b205477ce4414dd2e1
WooJongSeon <jswoo030@gmail.com>
2020-06-13 15:14:33 +0900

    promise 예제 추가

---
+++ javascript/promise/promise.then.js
@@ -0,0 +1,10 @@
+setTimeout(function() { // (A)
+  console.log('A');
+}, 0);
+Promise.resolve().then(function() { // (B)
+  console.log('B');
+}).then(function() { // (C)
+  console.log('C');
+}).then(() => {
+  console.log('D');
+})
