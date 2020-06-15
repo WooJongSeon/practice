130fdbd77f827facae487ebf770b2906527cf6e2
WooJongSeon <jswoo030@gmail.com>
2020-06-15 17:10:36 +0900

    내용 추가

--- javascript/native/native.js
+++ javascript/native/native.js
@@ -1,3 +1,9 @@
 const a = 'name';
 console.log(a.length);
 console.log(a.toString());
+
+const falsy1 = Boolean(false);
+const falsy2 = Number(0);
+const falsy3 = String('');
+const b = Boolean(falsy1 && falsy2 && falsy3);
+console.log(b);
