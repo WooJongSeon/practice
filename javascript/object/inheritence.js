16fb1ed5cd965a1c48b3237b5e973bb8a14d9e88
WooJongSeon <jswoo030@gmail.com>
2020-06-16 14:36:08 +0900

    내용 추가

---
+++ javascript/object/inheritence.js
@@ -0,0 +1,18 @@
+class Vehicle {
+  // constructor() { 여기 상위 클래스에만 constructor를 정의해야 한다.
+  //   console.log('vehicle')
+  // }
+}
+
+class Car extends Vehicle { // new 하면 상위 클래스의 constructor를 사용한다.
+  constructor() { // 이걸 상위 하위 두 군데에 정의하면 에러가 발생한다.
+    console.log('car');
+  }
+
+}
+
+class SpeedBoat extends Vehicle {
+
+}
+
+const car = new Car();
