e9b234a4342b255023f514e33e95586d0b7370eb
WooJongSeon <jswoo030@gmail.com>
2020-07-10 12:25:01 +0900

    code

--- 프로그래머스_고득점kit/DFS-BFS/타겟넘버.js
+++ 프로그래머스_고득점kit/DFS-BFS/타겟넘버.js
@@ -1,20 +1,20 @@
 function solution(numbers, target) {
-    var answer = 0;
-    function recur(idx, sum) {
-        if (idx === numbers.length) {
-            if (sum === target) {
-                answer += 1;
-            }
-            return;
-        }
-
-        recur(idx + 1, sum + numbers[idx]);
-        recur(idx + 1, sum - numbers[idx]);
+  var answer = 0;
+  function recur(idx, sum) {
+    if (idx === numbers.length) {
+      if (sum === target) {
+        answer += 1;
+      }
+      return;
     }
 
-    recur(0, 0);
+    recur(idx + 1, sum + numbers[idx]);
+    recur(idx + 1, sum - numbers[idx]);
+  }
 
-    return answer;
+  recur(0, 0);
+
+  return answer;
 }
 // 트리 구조를 활용해서 푼다.
 // 배열을 앞에서부터 하나씩 더했을 경우, 뺀 경우로 나눠서 트리를 뻗는다.
--- 프로그래머스_고득점kit/완전탐색/모의고사.js
+++ 프로그래머스_고득점kit/완전탐색/모의고사.js
@@ -1,5 +1,5 @@
 
-// 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/42840
+// 문제 링크: https://programmers.co.kr/le풀arn/courses/30/lessons/42840
 // 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
 //      1,2,3,4,5 반복
 // 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
@@ -7,33 +7,33 @@
 // 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
 //      3,3,1,1,2,2,4,4,5,5, 반복
 function solution(answers) {
-    let number1 = [1, 2, 3, 4, 5];
-    let number2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    let number3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    for (let i = 0; i < answers.length / 5; i++) {
-        number1.push(...[1, 2, 3, 4, 5]);
-        number2.push(...[2, 1, 2, 3, 2, 4, 2, 5]);
-        number3.push(...[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]);
+  let number1 = [1, 2, 3, 4, 5];
+  let number2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  let number3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  for (let i = 0; i < answers.length / 5; i++) {
+    number1.push(...[1, 2, 3, 4, 5]);
+    number2.push(...[2, 1, 2, 3, 2, 4, 2, 5]);
+    number3.push(...[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]);
+  }
+  let count = [0, 0, 0];
+  answers.forEach((value, index) => {
+    if (value === number1[index]) {
+      count[0] += 1;
     }
-    let count = [0, 0, 0];
-    answers.forEach((value, index) => {
-        if (value === number1[index]) {
-            count[0] += 1;
-        }
-        if (value === number2[index]) {
-            count[1] += 1;
-        }
-        if (value === number3[index]) {
-            count[2] += 1;
-        }
-    });
-    //최대값
-    var max = count.reduce(function (previous, current) {
-        return previous > current ? previous : current;
-    });
-    let answer = count.map((value, index) => { if (value === max) { return index + 1 } })
-        .filter((value) => { return value !== undefined });
-    return answer;
+    if (value === number2[index]) {
+      count[1] += 1;
+    }
+    if (value === number3[index]) {
+      count[2] += 1;
+    }
+  });
+  //최대값
+  var max = count.reduce(function (previous, current) {
+    return previous > current ? previous : current;
+  });
+  let answer = count.map((value, index) => { if (value === max) { return index + 1 } })
+    .filter((value) => { return value !== undefined });
+  return answer;
 }
 
 solution([1, 2, 3, 4, 5]);
