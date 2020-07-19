6be040063932eeb4d741147da29b92ce9013df48
WooJongSeon <jswoo030@gmail.com>
2020-07-20 00:03:40 +0900

    고득점 kit - 정렬 h-index 문제

---
+++ 프로그래머스_고득점kit/정렬/h-index.js
@@ -0,0 +1,42 @@
+// 문제의 규칙
+// h index의 최대값은 citations 배열의 크기를 넘을 수 없다
+
+function solution(citations) {
+  citations = citations.sort((a, b) => b - a); // 정렬한다.
+  if (citations[0] === 0) {
+    return 0;
+  }
+  if (citations[citations.length - 1] >= citations.length) {
+    return citations.length;
+  }
+  for (let index = 0; index < citations.length; index++) {
+    if (index >= citations[index]) {
+      return index;
+    }
+  }
+}
+// case 1 일반적인 경우
+// 6 5 3 1 0
+// 0 1 2 3 4
+
+// case 2 모두 같은 숫자의 경우
+// 5 5 5 5 5
+// 0 1 2 3 4 5?
+//
+
+//case 3 0인 경우
+// 0 return 0
+// 0
+
+console.log(solution([3, 0, 6, 1, 5]))
+console.log(solution([5, 5, 5, 5, 5]))
+console.log(solution([2, 2, 2, 2, 2]))
+// 0,0,0,0 => 0
+// 9,9,9,9 => 9 = 안됨,  4이상 4편 => 4가 답
+
+// 3 0 6 1 5
+// 0이상이 0개 이상이고 나머지가 0개 이하인가 ? 아님
+// 1이상이 1개 이상이고 나머지가 1개 이하인가 ? 아님
+// 2이상이 2개 아니다
+// 3이상이 3개인가 맞다 done
+// n이상이 n개인가? done
--- 프로그래머스_고득점kit/정렬/가장큰수.js
+++ 프로그래머스_고득점kit/정렬/가장큰수.js
@@ -1,6 +1,7 @@
 function solution(numbers) {
   const arr = numbers.map((n) => n + '') // 문자열로 바꾸기
     .sort((a, b) => (b + a) - (a + b)) // 조합한 숫자들을 내림차순으로 정렬
+  // a 가 뒤에있는 숫자, b가 앞에 있는 숫자
   return arr[0] === '0' ? '0' : arr.join(''); // 배열의 모든 요소가 0인경우 처리
 }
 // 배열에서 만들어 낼 수 있는 가장 큰 수
