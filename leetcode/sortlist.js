3f968f02e88c1f010d9328fec581cf693866c4c1
WooJongSeon <jswoo030@gmail.com>
2020-07-26 14:23:57 +0900

    내용 추가

--- leetcode/sortlist.js
+++ leetcode/sortlist.js
@@ -1,11 +1,11 @@
-function sortList (head) {
-  if(!head || !head.next) return head;
+function sortList(head) {
+  if (!head || !head.next) return head;
   // {} 빈 리스트인 경우 리스트 리턴
   let fast = head, slow = head;
   // 리스트를 각각 할당
-  while(fast.next && fast.next.next) {
-      fast = fast.next.next;
-      slow = slow.next;
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
   }
   let middle = slow.next;
   slow.next = null;
@@ -13,13 +13,14 @@ function sortList (head) {
 }
 const merge = (o, t) => {
   let d = new ListNode(-1); //dummy and temp
+
   let tmp = d;
-  while(o && t) {
-      tmp.next = (o.val < t.val) ? o : t;
-      tmp = tmp.next;
-      if(o.val < t.val) o = o.next; else t = t.next;
+  while (o && t) {
+    tmp.next = (o.val < t.val) ? o : t;
+    tmp = tmp.next;
+    if (o.val < t.val) o = o.next; else t = t.next;
   }
-  if(o) tmp.next = o;
-  if(t) tmp.next = t;
+  if (o) tmp.next = o;
+  if (t) tmp.next = t;
   return d.next;
 }
