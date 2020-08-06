bfb2025b8cbfb270d9b2094723b75054daf01e31
WooJongSeon <jswoo030@gmail.com>
2020-08-06 21:35:42 +0900

    내용 추가

---
+++ leetcode/94.Binary Tree Inorder Traversal.ts
@@ -0,0 +1,32 @@
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function inorderTraversal(root: TreeNode | null): number[] {
+  console.log(root)
+
+  return []
+};
+
+const treenode = new TreeNode(1, new TreeNode(1));
+console.log(inorderTraversal(treenode))
