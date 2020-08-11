1d34b48477507b526daeecd154eb6bc89c1933e0
WooJongSeon <jswoo030@gmail.com>
2020-08-11 13:59:49 +0900

    내용 추가

--- typescript/type.ts
+++ typescript/type.ts
@@ -1,32 +1,11 @@
-let x: [string, number];
-
-x = ['hello', 10];
-
-enum Color {
-  Red = 1, Green, Blue
+interface SquareConfig {
+  color?: string;
+  area?: number;
+  [propName: string]: any; // 여러개의 프로퍼티를 가질 수 있다.
+}
+function createSquare(config: SquareConfig): { color: string, area: number } {
+  return { color: '', area: 2 }
 }
 
-console.log(Color[1]);
+let mySquare = createSquare({ colour: 'adfasd', width: 100 } as SquareConfig);
 
-let list = [1, 'df', true, false, [1, 2, 3]]
-console.log(list);
-
-const objectFunc = (o: object) => {
-
-}
-
-let someValue: any = 'this is string';
-
-let strLength: number = (<string>someValue).length;
-
-(<string>someValue).length;
-
-
-interface Temp {
-  name: string;
-  age: number;
-}
-let temp: any = {};
-
-(<Temp>temp).age; // as 와 같은 역할을 한다. type assertions
-(temp as Temp).age;
